2285f18583b1c9470eba1363277b495abceac390
tanushabsc <tanusham.bsc23@rvu.edu.in>
2026-09-22 10:30:36 +0530

    Testing Success

--- inference.py
+++ inference.py
@@ -1,8 +1,6 @@
-# def predict(value):
-#     if value > 0.5:
-#         return "Positive"
-#     else:
-# 	    return "Negative"
-
 def predict(value):
-    return "Positive"
+    if value > 0.5:
+        return "Positive"
+    else:
+	    return "Negative"
+
